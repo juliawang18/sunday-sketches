bee87021bb83e320ab5b7b1dd9147b57cafc62a7
gaoag <keming.gao@berkeley.edu>
2022-11-06 15:52:10 -0800

    add features lol

--- js/canvas.js
+++ js/canvas.js
@@ -2,11 +2,21 @@
 let canvas = new fabric.Canvas('canvas', {
     isDrawingMode: false
 });
+canvas.setBackgroundColor('white');
 
 canvas.setHeight(window.innerHeight);
 canvas.setWidth(window.innerWidth);
 
 // Add image of the week
+let imageWidth = canvas.getWidth()/4
+fabric.Image.fromURL('https://raw.githubusercontent.com/juliawang18/sunday-sketches/main/imgs/sunday_header.png', function(img) {
+    img.scaleToWidth(imageWidth);
+    let imageHeight = img.getScaledHeight()
+
+    img.top = window.innerHeight/2 - imageHeight/2
+    img.left = window.innerWidth/2 - imageWidth/2
+    canvas.add(img);
+});
 
 
 // Initializing tool buttons
@@ -29,17 +39,46 @@ function toggleSelect() {
     canvas.isDrawingMode = false;
 }
 
+function downloadImage(data, filename) {
+    var a = document.createElement('a');
+    a.href = data;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    a.remove();
+}
+
 // Adding download to menu
 let saveButton = document.getElementById("save");
-
 saveButton.addEventListener(
     "click",
     function (e) {
-        this.href = canvas.toDataURL({
-            format: "png"
+        let link = document.createElement('a');
+        let canvas = document.getElementById('canvas');
+        let dataURL = canvas.toDataURL("image/jpeg");
+        downloadImage(dataURL, 'sundaysketch.jpeg')
+    }
+);
+
+
+// Adding a copy and paste function - add just the clipboard
+let copyButton = document.getElementById("copy");
+copyButton.addEventListener(
+    "click",
+    function (e) {
+        canvas.getElement().toBlob(function(blob) { 
+            const item = new ClipboardItem({ "image/png": blob });
+            navigator.clipboard.write([item]); 
         });
 
-        this.download = "sundaysketch.png";
-    },
-    false
+    }
 );
+
+copyButton.addEventListener(
+    "click",
+    function(e) {
+        copyButton.style.backgroundColor = '#FFF6D5';
+        setTimeout(() => { copyButton.style.backgroundColor = '#ffc800'; }, 450);
+    }
+)
+
